6698b4f87588b0d7014a5a452eedf494be0ed84d
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-02-23 21:49:40 +0100

    Menú maquetado en desktop

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,26 @@
 <template>
     <div id="nav" class="contenedor-menu">
-        <router-link to="/login">{{item1}}</router-link>
-        <router-link to="/registro">{{item2}}</router-link>
+        <div>
+            <router-link to="/"><img class="icono" src="../assets/img/menu-icon-2.png" /></router-link>
+        </div>
+        <div class="box-items-menu">
+            <router-link to="/rutas">{{item3}}</router-link>
+            <router-link to="/social">{{item4}}</router-link>
+            <router-link to="/mis-rutas">{{item5}}</router-link>
+            <router-link to="/estadisticas">{{item6}}</router-link>
+            <router-link to="/clasificaciones">{{item7}}</router-link>
+            <router-link to="/sorteos">{{item8}}</router-link>
+            <router-link to="/productos">{{item9}}</router-link>
+            <router-link to="/perfil">{{item10}}</router-link>
+        </div>
+        <div class="box-login-reg">
+            <router-link to="/login">
+                <div class="login-btn">{{item1}}</div>
+            </router-link>
+            <router-link to="/registro">
+                <div class="login-btn">{{item2}}</div>
+            </router-link>
+        </div>
     </div>
     <router-view/>
 </template>
@@ -12,7 +31,15 @@ export default {
     data() {
         return {
             item1: "Login",
-            item2: "Registro"
+            item2: "Registro",
+            item3: "Rutas",
+            item4: "Social",
+            item5: "Mis Rutas",
+            item6: "Estadísticas",
+            item7: "Clasificaciones",
+            item8: "Sorteos",
+            item9: "Productos",
+            item10: "Mi perfil"
         }
     }
 }
