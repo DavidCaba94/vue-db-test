39a245111a764a1f3c43756ab426962522d10130
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-02-26 21:59:30 +0100

    Registro a medio gas (falta encriptación, mensajes de error y redirección final)

---
+++ src/api/usuario.php
@@ -0,0 +1,63 @@
+<?php
+include_once 'conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$_POST = json_decode(file_get_contents("php://input"), true);
+
+function permisos() {  
+  if (isset($_SERVER['HTTP_ORIGIN'])){
+      header("Access-Control-Allow-Origin: *");
+      header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+      header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept");
+      header('Access-Control-Allow-Credentials: true');      
+  }  
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))          
+        header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept");
+    exit(0);
+  }
+}
+permisos();
+
+
+$opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
+$id = (isset($_POST['id'])) ? $_POST['id'] : '';
+$nombre = (isset($_POST['nombre'])) ? $_POST['nombre'] : '';
+$apellidos = (isset($_POST['apellidos'])) ? $_POST['apellidos'] : '';
+$email = (isset($_POST['email'])) ? $_POST['email'] : '';
+$password = (isset($_POST['password'])) ? $_POST['password'] : '';
+$fecha_registro = (isset($_POST['fecha_registro'])) ? $_POST['fecha_registro'] : '';
+$provincia = (isset($_POST['provincia'])) ? $_POST['provincia'] : '';
+$foto = (isset($_POST['foto'])) ? $_POST['foto'] : '';
+
+
+switch($opcion){
+	case 1:
+        $consulta = "SELECT * FROM usuarios";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;
+    case 2:
+        $consulta = "INSERT INTO usuarios (nombre, apellidos, email, passwd, fecha_registro, provincia) VALUES('$nombre', '$apellidos', '$email', 'md5($password)', '$fecha_registro', '$provincia') ";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();                
+        break;
+    case 3:
+        $consulta = "UPDATE usuarios SET nombre='$nombre', apellidos='$apellidos', email='$email', passwd='$password', foto='$foto', provincia='$provincia' WHERE id='$id' ";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();                        
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+        break;        
+    case 4:
+        $consulta = "DELETE FROM usuarios WHERE id='$id' ";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();                           
+        break;         
+    
+}
+print json_encode($data, JSON_UNESCAPED_UNICODE);
+$conexion = NULL;
--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -1,13 +1,144 @@
 <template>
-  Registro
+  <div class="contenedor-registro">
+    <div class="box-form">
+      <router-link to="/"><img class="logo-form" src="../assets/img/menu-icon-2.png"></router-link>
+      <div class="form__group field">
+        <input type="input" class="form__field" placeholder="Nombre" name="nombre" id='nombre' required />
+        <label for="nombre" class="form__label">Nombre</label>
+      </div>
+      <div class="form__group field">
+        <input type="input" class="form__field" placeholder="Apellidos" name="apellidos" id='apellidos' required />
+        <label for="apellidos" class="form__label">Apellidos</label>
+      </div>
+      <div class="form__group field">
+        <input type="email" class="form__field" placeholder="Email" name="email" id='email' required />
+        <label for="email" class="form__label">Email</label>
+      </div>
+      <div class="form__group field">
+        <input type="password" class="form__field" placeholder="Contraseña" name="password" id='password' required />
+        <label for="password" class="form__label">Contraseña</label>
+      </div>
+      <div class="form__group field">
+        <input type="password" class="form__field" placeholder="Confirmar contraseña" name="password-confirm" id='password-confirm' required />
+        <label for="password-confirm" class="form__label">Confirmar contraseña</label>
+      </div>
+      <div class="form__group field">
+        <input type="input" class="form__field" placeholder="Provincia" name="provincia" id='provincia' required />
+        <label for="provincia" class="form__label">Provincia</label>
+      </div>
+      <div class="btn-registrar" @click="comprobarRegistro()">Registrar</div>
+      <div id="loading-reg" class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    </div>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-export default {
 
+import axios from "axios";
+
+var url = "http://alcortewear.es/post/rest/grupetapp/usuario.php";
+
+export default {
+      data () {
+        return {            
+                    
+        }
+      }, 
+      methods:{
+          comprobarRegistro:function(){
+            this.desmarcarErrores();
+            var todoRelleno = true;
+
+            window.$(".btn-registrar").css("display", "none");
+            window.$("#loading-reg").css("display", "block");
+
+            var nombre = window.$("#nombre").val();
+            var apellidos = window.$("#apellidos").val();
+            var email = window.$("#email").val();
+            var password = window.$("#password").val();
+            var passwordConfirm = window.$("#password-confirm").val();
+            var provincia = window.$("#provincia").val();
+
+            if(nombre == ""){
+              todoRelleno = false;
+              this.marcarVacio(window.$("#nombre"));
+            }
+
+            if(apellidos == ""){
+              todoRelleno = false;
+              this.marcarVacio(window.$("#apellidos"));
+            }
+
+            if(email == ""){
+              todoRelleno = false;
+              this.marcarVacio(window.$("#email"));
+            }
+
+            if(password == ""){
+              todoRelleno = false;
+              this.marcarVacio(window.$("#password"));
+            }
+
+            if(passwordConfirm == ""){
+              todoRelleno = false;
+              this.marcarVacio(window.$("#password-confirm"));
+            }
+
+            if(provincia == ""){
+              todoRelleno = false;
+              this.marcarVacio(window.$("#provincia"));
+            }
+
+            if(todoRelleno){
+              this.comprobarPassword(nombre, apellidos, email, password, passwordConfirm, provincia);
+            } else {
+              window.$(".btn-registrar").css("display", "block");
+              window.$("#loading-reg").css("display", "none");
+            }
+          },
+          comprobarPassword:function(nombre, apellidos, email, password, passwordConfirm, provincia){
+            if(password == passwordConfirm){
+              this.registrar(nombre, apellidos, email, password, provincia);
+            } else {
+              this.marcarVacio(window.$("#password"));
+              this.marcarVacio(window.$("#password-confirm"));
+              window.$(".btn-registrar").css("display", "block");
+              window.$("#loading-reg").css("display", "none");
+            }
+          },
+          marcarVacio:function(elemento){
+            elemento.css("background-color", "#ff000026");
+          },
+          desmarcarErrores:function(){
+            window.$("#nombre").css("background-color", "#ffffff");
+            window.$("#apellidos").css("background-color", "#ffffff");
+            window.$("#email").css("background-color", "#ffffff");
+            window.$("#password").css("background-color", "#ffffff");
+            window.$("#password-confirm").css("background-color", "#ffffff");
+            window.$("#provincia").css("background-color", "#ffffff");
+          },
+          registrar:function(nombre, apellidos, email, password, provincia){
+              axios.post(url, {
+                opcion:2, 
+                nombre: nombre, 
+                apellidos: apellidos, 
+                email: email, 
+                password: password,
+                fecha_registro: new Date().toJSON().slice(0, 10),
+                provincia: provincia
+              }).then(response =>{
+                if(response.statusText == "OK"){
+                  console.log("Registro correcto");
+                }
+                console.log(response);
+              });
+          }
+      }
 }
+
 </script>
 
 <style>
-
+@import '../assets/css/registro.css';
 </style>
