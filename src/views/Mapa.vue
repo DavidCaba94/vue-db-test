0fb50742fdc640ea72b6748a7e025dba6502e199
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-04-04 14:52:05 +0200

    Mapa funcionando

--- src/views/Mapa.vue
+++ src/views/Mapa.vue
@@ -1,8 +1,9 @@
 <template>
-  Mapa
+  <div id="mapid"></div>
 </template>
 
 <script>
+import L from 'leaflet';
 export default {
     name: "Mapa",
     data () {
@@ -12,10 +13,19 @@ export default {
     },
     mounted() {
         if(localStorage.usuario) this.usuario = JSON.parse(localStorage.usuario);
+        var mymap = L.map('mapid').setView([40.41680911937625, -3.703799935080432], 6);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Grupetapp',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZGF2aWRjYWJhOTQiLCJhIjoiY2tuMzVmNHdvMDRsajJ5cXJ3YmZldmo4aSJ9.s-GOpcUEx7q6JPSjyOMPpQ'
+        }).addTo(mymap);
     }
 }
 </script>
 
 <style>
-
+#mapid { height: 760px; }
 </style>
