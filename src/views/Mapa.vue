5af405034f1e1233749ea0ef189d793125c94ecf
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-04-05 21:21:28 +0200

    Avances mapas con markers custom y mapas en cards de mis rutas

--- src/views/Mapa.vue
+++ src/views/Mapa.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="mapid"></div>
+    <div class="contenedor-mapa">
+        <div class="filtros-mapa">
+            <input type="text" placeholder="Tipo de ruta">
+        </div>
+        <div id="mapid"></div>
+    </div>
 </template>
 
 <script>
+
 import L from 'leaflet';
+
+var mymap;
+
 export default {
     name: "Mapa",
     data () {
@@ -13,7 +22,7 @@ export default {
     },
     mounted() {
         if(localStorage.usuario) this.usuario = JSON.parse(localStorage.usuario);
-        var mymap = L.map('mapid').setView([40.41680911937625, -3.703799935080432], 6);
+        mymap = L.map('mapid').setView([40.41680911937625, -3.703799935080432], 6);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Grupetapp',
             maxZoom: 18,
@@ -22,10 +31,33 @@ export default {
             zoomOffset: -1,
             accessToken: 'pk.eyJ1IjoiZGF2aWRjYWJhOTQiLCJhIjoiY2tuMzVmNHdvMDRsajJ5cXJ3YmZldmo4aSJ9.s-GOpcUEx7q6JPSjyOMPpQ'
         }).addTo(mymap);
+
+        
+        mymap.on('click', this.onMapClick);
+
+        this.addMarkers();
+    },
+    methods: {
+        addMarkers:function() {
+            var customMarker = L.icon({
+                iconUrl: require('../assets/img/favicon.png'),
+                iconSize:     [38, 38],
+                iconAnchor:   [19, 38]
+            });
+
+            L.marker([40.958471, -5.6582],{icon: customMarker}).addTo(mymap)
+                .bindPopup('Descripción marcador mapa');
+        },
+        onMapClick:function(e) {
+            L.popup()
+                .setLatLng(e.latlng)
+                .setContent("Has picado en las coordenadas " + e.latlng.toString())
+                .openOn(mymap);
+        }
     }
 }
 </script>
 
 <style>
-#mapid { height: 760px; }
+@import '../assets/css/mapa.css';
 </style>
--- src/views/MisRutas.vue
+++ src/views/MisRutas.vue
@@ -7,7 +7,7 @@
       <div id="box-rutas-activas" class="box-rutas">
           <router-link :to="'/ruta/1'">
             <div class="card-ruta">
-                <div class="capa-mapa"></div>
+                <div id="mapid" class="capa-mapa"></div>
                 <p class="nombre-ruta">Nombre Ruta</p>
                 <div class="box-parametros">
                     <div>
@@ -44,7 +44,7 @@
       <div id="box-rutas-pasadas" class="box-rutas">
           <router-link :to="'/ruta/1'">
             <div class="card-ruta">
-                <div class="capa-mapa"></div>
+                <div class="capa-mapa-generico"></div>
                 <p class="nombre-ruta">Nombre Ruta</p>
                 <div class="box-parametros">
                     <div>
@@ -80,14 +80,34 @@
 </template>
 
 <script>
+import L from 'leaflet';
+
 export default {
     data () {
         return {
-            usuario: ''
+            usuario: '',
+            mymap: null
         }
     },
     mounted() {
         if(localStorage.usuario) this.usuario = JSON.parse(localStorage.usuario);
+
+        this.mymap = L.map('mapid').setView([40.958471, -5.6582], 12);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Grupetapp',
+            maxZoom: 18,
+            zoomControl: false,
+            attributionControl: false,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZGF2aWRjYWJhOTQiLCJhIjoiY2tuMzVmNHdvMDRsajJ5cXJ3YmZldmo4aSJ9.s-GOpcUEx7q6JPSjyOMPpQ'
+        }).addTo(this.mymap);
+
+        
+        this.mymap.on('click', this.onMapClick);
+
+        this.addMarkers();
     },
     methods: {
         toggleRutasActivas:function() {
@@ -95,6 +115,22 @@ export default {
         },
         toggleRutasPasadas:function() {
             window.$("#box-rutas-pasadas").slideToggle();
+        },
+        addMarkers:function() {
+            var customMarker = L.icon({
+                iconUrl: require('../assets/img/favicon.png'),
+                iconSize:     [38, 38],
+                iconAnchor:   [19, 38]
+            });
+
+            L.marker([40.958471, -5.6582],{icon: customMarker}).addTo(this.mymap)
+                .bindPopup('Descripción marcador mapa');
+        },
+        onMapClick:function(e) {
+            L.popup()
+                .setLatLng(e.latlng)
+                .setContent("Has picado en las coordenadas " + e.latlng.toString())
+                .openOn(this.mymap);
         }
     }
 }
