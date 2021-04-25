a139d6201234c5ab1854028b41413ec07a6acf3b
DavidCaba6194 <cabayosoyasi@gmail.com>
2021-04-25 14:37:51 +0200

    Home a medio gas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,45 @@
 <template>
     <div class="contenedor-home">
-        <router-link to="/login">
-            <div class="login-btn">Entrar</div>
-        </router-link>
-        <router-link to="/registro">
-            <div class="login-btn">Registro</div>
-        </router-link>
+        <div class="cabecera-home">
+            <div>
+                <img class="icono-cabecera" src="../assets/img/menu-icon-2.png">
+            </div>
+            <div>
+                <router-link to="/login">
+                    <div class="login-btn">Entrar</div>
+                </router-link>
+                <router-link to="/registro">
+                    <div class="login-btn">Registro</div>
+                </router-link>
+            </div>
+        </div>
+        <div class="fondo-banner-home">
+            <div class="banner-home">
+                <div>
+                    <p class="titulo-banner">Bienvenido a Grupetapp</p>
+                    <p class="subtitulo-banner">La red social creada por y para ciclistas</p>
+                </div>
+            </div>
+        </div>
+        <p class="titulo-tarjetas">Con Grupetapp puedes</p>
+        <div class="tarjetas-descriptivas">
+            <div class="tarjeta">
+                <div class="img-tarjeta img-1"></div>
+                <div class="texto-tarjeta">Apuntarte y asistir a rutas creadas por otros ciclistas o crear las tuyas propias</div>
+            </div>
+            <div class="tarjeta">
+                <div class="img-tarjeta img-2"></div>
+                <div class="texto-tarjeta">Conocer gente con tu misma pasión y rodar juntos</div>
+            </div>
+            <div class="tarjeta">
+                <div class="img-tarjeta img-3"></div>
+                <div class="texto-tarjeta">Crear tus propias publicaciones y compartir tus experiencias con otros ciclistas</div>
+            </div>
+            <div class="tarjeta">
+                <div class="img-tarjeta img-4"></div>
+                <div class="texto-tarjeta">Descubrir nuevas rutas en cada quedada o evento</div>
+            </div>
+        </div>
         <h2>Número de usuarios: {{ this.numUsuarios }}</h2>
         <h2>Número de publicaciones: {{ this.numPublicaciones }}</h2>
         <h2>Número de rutas: {{ this.numRutas }}</h2>
@@ -60,17 +94,5 @@ export default {
 </script>
 
 <style>
-.contenedor-home .login-btn {
-    width: 100px;
-    background-color: #CC5454;
-    color: #ffffff;
-    border-radius: 10px;
-    padding: 10px;
-    margin-right: 10px;
-    cursor: pointer;
-}
-
-.contenedor-home .login-btn:hover {
-    background-color: #a73e3e;
-}
+@import '../assets/css/home.css';
 </style>
